6ec9dfd345e6dc7f4cf4b3df1b3f709b7b0e7678
adarshss006 <36719530+adarshss006@users.noreply.github.com>
2025-04-02 22:57:40 +0200

    Update bringup_phase3.sh

--- bringup_phase3.sh
+++ bringup_phase3.sh
@@ -7,5 +7,6 @@ MAP_FILE="$WORKSPACE/src/my_nav2_pkg/config/maze_map_save.yaml"
 tmux new-session -d -s $SESSION -n ekf "source $WORKSPACE/install/setup.bash && ros2 launch my_robot_description ekf_launch.py"
 tmux new-window -t $SESSION -n nav2_local "source $WORKSPACE/install/setup.bash && ros2 launch my_nav2_pkg localization_launch.py map:=$MAP_FILE"
 tmux new-window -t $SESSION -n nav2_nav "source $WORKSPACE/install/setup.bash && ros2 launch my_nav2_pkg/navigation_launch.py"
+tmux new-window -t $SESSION -n nav2_nav "source $WORKSPACE/install/setup.bash && ros2 run yolov8_ros yolov8_node -- device 0"
 
 tmux attach -t $SESSION
